d42c8002979c9e272ea96d244a540a61bf69a61f
Austin Rutledge <austinrutledge0@gmail.com>
2020-04-21 15:23:54 -0700

    first version of contact form done

--- src/components/Contact/ContactForm.tsx
+++ src/components/Contact/ContactForm.tsx
@@ -1,22 +1,58 @@
 import React from "react";
 import makeStyles from "@material-ui/core/styles/makeStyles";
-import { TextField } from "@material-ui/core";
+import {Button, TextField} from "@material-ui/core";
 
 const styles = makeStyles({
-  contactForm:{
+  contactForm: {
     height: 500,
-    display:"flex",
-    width:"50%",
+    display: "flex",
+    width: "50%",
     borderTop: "solid 0px transparent",
     borderLeft: "solid 0px transparent",
     borderRight: "solid 0px transparent",
     borderBottom: "solid 0px transparent",
     boxShadow: "0 0 5px #333",
-    justifyContent:"center",
-    alignItems:"center",
-    marginTop:25,
-    marginBottom:25
-  }
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 25,
+    marginBottom: 25
+  },
+  smallTextInputSpacing:{
+    paddingRight: 25
+  },
+  largeTextInput:{
+    marginTop: 25,
+    width: "100%"
+  },
+  pageHeader: {
+    alignItems: "center",
+    justifyContent: "center",
+    display: "flex",
+    flexWrap: "wrap"
+  },
+  submitButton:{
+    backgroundColor:"#FF40FF"
+  },
+  submitButtonContainer:{
+    display: "flex",
+    flexDirection:"row-reverse",
+    justifyContent: "right",
+    paddingTop:15
+  },
+  "@media (max-width: 600px)": {
+    contactForm: {
+      width: "100%"
+    },
+    smallTextInputSpacing:{
+      paddingRight: 0
+    },
+    largeTextInput:{
+      width: "75%"
+    },
+    submitButton:{
+      marginRight: 40
+    }
+  },
 });
 
 export const ContactForm = () => {
@@ -24,54 +60,27 @@ export const ContactForm = () => {
   return (
     <div className={classes.contactForm}>
       <form noValidate autoComplete="off">
+        <div className={classes.pageHeader}>
+          <h1>Contact Us</h1>
+        </div>
         <div>
-          <TextField
-            error
-            id="standard-error"
-            label="Error"
-            defaultValue="Hello World"
-          />
-          <TextField
-            error
-            id="standard-error-helper-text"
-            label="Error"
-            defaultValue="Hello World"
-            helperText="Incorrect entry."
-          />
+          <TextField required id="standard-required" label="Name" className={classes.smallTextInputSpacing} />
+          <TextField required id="standard-required" label="Email"/>
         </div>
         <div>
           <TextField
-            error
-            id="filled-error"
-            label="Error"
-            defaultValue="Hello World"
-            variant="filled"
-          />
-          <TextField
-            error
-            id="filled-error-helper-text"
-            label="Error"
-            defaultValue="Hello World"
-            helperText="Incorrect entry."
-            variant="filled"
+            id="outlined-multiline-static"
+            label="Message"
+            multiline
+            rows={10}
+            variant="outlined"
+            className={classes.largeTextInput}
           />
         </div>
-        <div>
-          <TextField
-            error
-            id="outlined-error"
-            label="Error"
-            defaultValue="Hello World"
-            variant="outlined"
-          />
-          <TextField
-            error
-            id="outlined-error-helper-text"
-            label="Error"
-            defaultValue="Hello World"
-            helperText="Incorrect entry."
-            variant="outlined"
-          />
+        <div className={classes.submitButtonContainer}>
+        <Button variant="contained" color="primary" className={classes.submitButton}>
+          Submit
+        </Button>
         </div>
       </form>
     </div>
